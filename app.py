6254656e3ad7058d9428a2f10161ae33a6d13e64
Jason Howard <81219135+Jsonhwrd@users.noreply.github.com>
2022-12-03 02:33:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 import os
-from random import randint
 
-for i in range(1, 365):
 
-    for j in range(0, randint(1, 10)):
-        d = str(i) + 'days ago'
-        with open('file.txt', 'a') as file:
-            file.write(d)
-        os.system('git add .')
-        os.system('git commit --date="' + d + '"  -m  "commit"')
+def make_commit(days: int):
+    if days < 1:
+        return os.system('git push')
 
-os.system('git push -u origin main')
+    else:
+        dates = f'{days} days ago'
+
+        with open('data.txt', 'a') as file:
+            file.write(f'{dates}\n')
+
+        os.system('git add data.txt')
+
+        os.system('git commit --date="'+dates+'" -m "First Commit')
+
+        return days * make_commit(days-1)
+
+        make_commit(265)
+
+       
